53a0b65077ffb7fa5cb03cdeaf11401f243b54b3
Anders Wiggers <anders.bille.wiggers@post.au.dk>
2019-05-13 12:27:25 +0200

    updated the res page

--- components/Resources/ResPage.js
+++ components/Resources/ResPage.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
-
 
 export default class ResPage extends Component {
 	constructor(props) {
@@ -34,7 +33,7 @@ export default class ResPage extends Component {
 	renderLoading() {
 		return (
 			<div className="loader">
-        <img src="https://yalantis.com/uploads/ckeditor/pictures/365/content_Loading-Loop-1.gif"></img>
+				<img src="https://yalantis.com/uploads/ckeditor/pictures/365/content_Loading-Loop-1.gif" />
 				<style jsx>{`
 					.loader {
 						text-align: center;
@@ -53,20 +52,19 @@ export default class ResPage extends Component {
 		return (
 			<div>
 				<div className="error">Sorry the requested resources is unvialbe</div>
-        <style jsx>{`
+				<style jsx>{`
 					.error {
 						table-layout: fixed;
 						width: 100%;
-            text-align: center;
+						text-align: center;
 						display: table;
-            line-height:80vh;
+						line-height: 80vh;
 						height: 100vh;
 					}
 				`}</style>
 			</div>
 		);
 	}
-
 
 	renderResources() {
 		const { error, content } = this.state;
@@ -76,110 +74,135 @@ export default class ResPage extends Component {
 			return this.renderError();
 		}
 
-		if (content === null){
+		if (content === null) {
 			return this.renderError();
 		}
 
 		return (
 			<div className="xs">
-        <h1>{content.resourceName}</h1>
-        <h3>{content.resourceDescription}</h3>
-        <p>{content.resourceDescription}</p>
+				<h1>{content.resourceName}</h1>
+				<h3>{content.resourceDescription}</h3>
+				<p>{content.resourceDescription}</p>
 
 				<style jsx>{`
 					.xs {
-						height:20px;
+						height: 20px;
 					}
 				`}</style>
 			</div>
 		);
 	}
 
-
-	renderFromBlock(){
+	renderFromBlock() {
 		const data = {
-			"time" : 1557690088396,
-			"blocks" : [
+			time: 1557690088396,
+			blocks: [
 				{
-					"type" : "header",
-					"data" : {
-						"text" : "Editor.js",
-						"level" : 2
+					type: 'header',
+					data: {
+						text: 'Editor.js',
+						level: 2
 					}
 				},
 				{
-					"type" : "paragraph",
-					"data" : {
-						"text" : "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text."
+					type: 'paragraph',
+					data: {
+						text: 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text.'
 					}
 				},
 				{
-					"type" : "header",
-					"data" : {
-						"text" : "Key features",
-						"level" : 3
+					type: 'header',
+					data: {
+						text: 'Key features',
+						level: 3
 					}
 				},
 				{
-					"type" : "list",
-					"data" : {
-						"style" : "unordered",
-						"items" : [
-							"It is a block-styled editor",
-							"It returns clean data output in JSON",
-							"Designed to be extendable and pluggable with a simple API"
+					type: 'list',
+					data: {
+						style: 'unordered',
+						items: [
+							'It is a block-styled editor',
+							'It returns clean data output in JSON',
+							'Designed to be extendable and pluggable with a simple API'
 						]
 					}
 				}
 			],
-			"version" : "2.12.4"
-		}
+			version: '2.12.4'
+		};
 
-		
-		let blocks = data.blocks
+		let blocks = data.blocks;
 
-		let html = []
+		let html = [];
 
-
-
-		blocks.forEach((block) =>{
-			console.log(block)
-			switch(block.type){
-				case "header":
-					html.push(<this.CustomTag>{block.data.text}</this.CustomTag>)
+		blocks.forEach((block) => {
+			console.log(block);
+			switch (block.type) {
+				case 'header':
+					html.push(<h1>{block.data.text}</h1>);
 					break;
-				case "paragraph":
-					html.push(<p>{block.data.text}</p>)
+				case 'paragraph':
+					html.push(<p>{block.data.text}</p>);
 					break;
-				case "list":
-					html.push(
-						<ul>
-							{block.data.items.map((item) => <li>{item}</li>)}
-						</ul>
-					)
+				case 'list':
+					html.push(<ul>{block.data.items.map((item) => <li>{item}</li>)}</ul>);
 			}
+		});
 
-		})
-
-		return (html)
-
+		return html;
 	}
 
-
-	CustomTag (hPoperty){ 
-		return `h${hPoperty}`
+	CustomTag(hPoperty) {
+		return `h${hPoperty}`;
 	}
 
 	render() {
 		return (
 			<div>
-				<div className="res">{this.state.loading ? this.renderLoading() : this.renderFromBlock()}</div>
+				<div className="outer">
+					<div className="res">{this.state.loading ? this.renderLoading() : this.renderFromBlock()}</div>
+				</div>
 				<style jsx>{`
+					h1 {
+						font-family: Lucida Grande, Lucida Sans Unicode, Lucida Sans;
+						letter-spacing: -.03em;
+						padding: 1em 0;
+						margin: 0;
+						margin-bottom: -0.9em;
+						line-height: 1.5em;
+						outline: none;
+						font-size: 1em;
+					}
+					.outer {
+						margin: 50px 0 50px 0;
+						background-color: aliceblue;
+						padding: 40px;
+						border-radius: 50px;
+					}
 					.res {
+						padding: 40px;
 						table-layout: fixed;
 						width: 100%;
 						display: table;
 						height: 86vh;
+						color: #313649;
+						background-color: white;
+						border-radius: 5px;
+						-webkit-box-shadow: 0px 0px 21px -5px rgba(0, 0, 0, 0.22);
+						-moz-box-shadow: 0px 0px 21px -5px rgba(0, 0, 0, 0.22);
+					}
+				`}</style>
+				<style jsx global>{`
+					h1 {
+						font-family: Lucida Grande, Lucida Sans Unicode, Lucida Sans;
+						letter-spacing: -.03em;
+						padding: 1em 0;
+						margin: 0;
+						margin-bottom: -0.9em;
+						line-height: 1.5em;
+						outline: none;
+						font-size: 2em;
 					}
 				`}</style>
 			</div>
