718d774ea18b39a8d95a14507b76ffe4bea44d15
Anders Wiggers <info@anderswiggers.dk>
2019-08-12 15:14:20 +0200

    testing release

--- pages/software.js
+++ pages/software.js
@@ -1,6 +1,8 @@
 import Layout from '../components/Layout/Layout.js';
 import Ripo from '../components/Software/Ripo';
 
+// release test
+
 const Software = () => (
 	<Layout title="Software" menu="soft">
 		<Ripo name="anders-wiggers" />
