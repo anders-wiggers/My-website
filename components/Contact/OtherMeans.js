202d724e6452bda918c8ffa94eea0c2a03197cd9
Anders Wiggers <anders.bille.wiggers@post.au.dk>
2019-08-19 13:23:24 +0200

    anallytics added

--- components/Contact/OtherMeans.js
+++ components/Contact/OtherMeans.js
@@ -6,15 +6,56 @@ export default class OtherMeans extends Component {
 		return (
 			<Element name="other">
 				<div className="otherContainer">
-					<h3>I have a sweet instagram</h3>
-					<h3>Maybe reach me on facebook?</h3>
-					<h3>Maybe LinkedIN?</h3>
-					<h3>Or just shoot an email at info@anderswiggers.dk</h3>
+					<div className="social">
+						<h3>
+							<a href="https://www.instagram.com/saintwiggers/" target="_blanc">
+								<i className="fab fa-instagram" /> I have a sweet instagram
+							</a>
+						</h3>
+						<h3>
+							<a href="https://www.facebook.com/anders.wiggers" target="_blanc">
+								<i className="fab fa-facebook-square" /> Howbout' facebook?
+							</a>
+						</h3>
+						<h3>
+							<a href="https://linkedin.com/in/anders-bille-wiggers-a656b9150/" target="_blanc">
+								<i className="fab fa-linkedin" /> Maybe LinkedIn?
+							</a>
+						</h3>
+						<h3>
+							<a href="https://linkedin.com/in/anders-bille-wiggers-a656b9150/" target="_blanc">
+								<i className="fab fa-github" /> GitHubs also a possiblity?
+							</a>
+						</h3>
+						<h3>
+							<a href="mailto:info@anderswiggers.dk">
+								<i className="fas fa-envelope-open" /> Or just shoot an email at info@anderswiggers.dk
+							</a>
+						</h3>
+					</div>
 					<style jsx>{`
+						a:hover {
+							text-decoration: none;
+							color: blue;
+						}
+						a:link {
+							color: #2a2e31;
+						}
+						a:visited {
+							color: #2a2e31;
+						}
+						.social {
+							background: #eaeaea;
+							padding: 30px;
+							border-radius: 6px;
+						}
 						.otherContainer {
 							min-height: 100vh;
-							background: green;
-							padding-top: 58px;
+							padding-top: 25vh;
+						}
+						h3 {
+							text-align: center;
+							margin: 30px;
 						}
 					`}</style>
 				</div>
--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,37 +1,54 @@
 import Navbar from '../Navbar';
 import Head from 'next/head';
 import Footer from '../footer/Footer';
+import { initGA, logPageView } from '../Utils/analytics';
+import React, { Component } from 'react';
 
-const Layout = (props) => (
-	<div style={body}>
-		<Head>
-			<title>{props.title}</title>
-			<link rel="icon" type="image/x-icon" href="/static/images/aw.png" />
-			<link
-				rel="stylesheet"
-				href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-				integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-				crossorigin="anonymous"
-			/>
-			<link
-				rel="stylesheet"
-				href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-				integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-				crossorigin="anonymous"
-			/>
+export default class Layout extends Component {
+	constructor(props) {
+		super(props);
+	}
 
-			<link
-				rel="stylesheet"
-				href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-			/>
-		</Head>
-		<Navbar menu={props.menu} />
-		<div className="container" style={defualtPadding}>
-			{props.children}
-		</div>
-		<Footer />
-	</div>
-);
+	componentDidMount() {
+		if (!window.GA_INITIALIZED) {
+			initGA();
+			window.GA_INITIALIZED = true;
+		}
+		logPageView();
+	}
+	render() {
+		return (
+			<div style={body}>
+				<Head>
+					<title>{this.props.title}</title>
+					<link rel="icon" type="image/x-icon" href="/static/images/aw.png" />
+					<link
+						rel="stylesheet"
+						href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+						integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+						crossorigin="anonymous"
+					/>
+					<link
+						rel="stylesheet"
+						href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+						integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+						crossorigin="anonymous"
+					/>
+
+					<link
+						rel="stylesheet"
+						href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+					/>
+				</Head>
+				<Navbar menu={this.props.menu} />
+				<div className="container" style={defualtPadding}>
+					{this.props.children}
+				</div>
+				<Footer />
+			</div>
+		);
+	}
+}
 
 const body = {
 	color: '#000',
@@ -48,5 +65,3 @@ const defualtPadding = {
 	minHeight: '100vh',
 	marginTop: '50px'
 };
-
-export default Layout;
